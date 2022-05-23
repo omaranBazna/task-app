b021bfc598def5ff7c976430a7ac9778c510f73a
omaranBazna <omran4d@gmail.com>
2022-05-22 21:50:30 -0400

    taks manger application using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+
+class App extends React.Component {
+
+
+  constructor(props) {
+  super(props);
+  this.state = {
+  tasks: [],
+  input: ""
+  };
+  }
+ 
+  render() {
+    
+let tasksArr=[];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+  <h1>Tasks</h1>
+  <ul>
+  <input onChange={this.handleChange} value={this.state.input} />
+  <button onClick={this.addTask}>Add Task</button>
+  <button onClick={this.removeTask}>Remove Task</button>
+  <button onClick={this.removeAll}>Remove All</button>
+  {this.state.tasks.map((task, i) =>
+  <li key={i}>
+  {task}
+  </li>
+  )}
+  </ul>
+  <div>
+  
+  </div>
+  </div>
   );
-}
+  }
+  
+  handleChange = (event) => {
+  this.setState({
+  input: event.target.value
+  });
+  }
+  
+  addTask = () => {
+
+      this.setState(previousState=>({
+        tasks:[...previousState.tasks,this.state.input],
+        input:""
+      
+      }))
+  }
+
+
+  removeTask= () =>{
+    this.setState((previousState)=>{
+      let oldTasks=[...previousState.tasks];
+      let newTasks=[];
+      for(let i=0;i<oldTasks.length;i++){
+        if(oldTasks[i] !==previousState.input){
+          newTasks.push(oldTasks[i]);
+        }
+      }
+      return (
+      
+      
+      {
+      
+      tasks:[...newTasks],
+      input:""
+    
+    })})
+  
+
+  }
+
+
+  
+  removeAll= () =>{
+    this.setState((previousState)=>{
+
+      return (
+      
+      
+      {
+      
+      tasks:[],
+      input:""
+    
+    })})
+  
+
+  }
+  
+  }
+  
+  
+  
 
 export default App;
+
+
+
